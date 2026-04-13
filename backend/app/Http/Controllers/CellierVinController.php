9f701d455fc197b602aeb12d31d9b93287fe976a
Jessica <jessica.stpierregagne@gmail.com>
2026-04-13 14:35:39 -0400

    creation du controlleur CellierVinController

---
+++ backend/app/Http/Controllers/CellierVinController.php
@@ -0,0 +1,65 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\CellierVin;
+use Illuminate\Http\Request;
+
+class CellierVinController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(CellierVin $cellierVin)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit(CellierVin $cellierVin)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Request $request, CellierVin $cellierVin)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(CellierVin $cellierVin)
+    {
+        //
+    }
+}
